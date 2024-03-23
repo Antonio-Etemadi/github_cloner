02b8f5d381d7b3d9dab67236d00261a6004e1968
Antonio-Etemadi <48334814+Antonio-Etemadi@users.noreply.github.com>
2024-03-23 04:03:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ try :
     from github_cloner import Ota_github
 except :
     import urequests as requests
-
-url = "https://raw.githubusercontent.com/Antonio-Etemadi/goithub_cloner/main/github_cloner.py"
-response = requests.get(url)
-
-if response.status_code == 200:
-    with open("github_cloner.py", "wb") as file:
-        file.write(response.content)
-    print("File downloaded successfully.")
+    url = "https://raw.githubusercontent.com/Antonio-Etemadi/goithub_cloner/main/github_cloner.py"
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open("github_cloner.py", "wb") as file:
+            file.write(response.content)
+        print("File downloaded successfully.")
+    else:
+        print("Failed to download the file.")
     
-else:
-    print("Failed to download the file.")
 from github_cloner import Ota_github
 url = "github.com/repos/Antonio-Etemadi/github_cloner"
 ota_instance = Ota_github(url)
